38ab68b4ec4f0e6ae4545de3696a9f48dd4d6a6f
BeastCP18 <pagetb@gonzaga.org>
2025-03-08 16:54:19 -0500

    Update About Us and HomePage styles; enhance UI with new images and color scheme

--- my-crud-app/HomePage.js
+++ my-crud-app/HomePage.js
@@ -1,14 +1,14 @@
 let floorImage = document.getElementById('floorImage');
-floorImage.src = 'Map of Gonzaga.jpg';
+floorImage.src = 'First Floor.jpg';
 
 document.getElementById('floor1').addEventListener('click', function() {
-    document.getElementById('floorImage').src = 'Map of Gonzaga.jpg';
+    document.getElementById('floorImage').src = 'First Floor.jpg';
 });
 
 document.getElementById('floor2').addEventListener('click', function() {
-    document.getElementById('floorImage').src = 'Magic the Gathering.webp';
+    document.getElementById('floorImage').src = 'Second Floor.jpg';
 });
 
 document.getElementById('floor3').addEventListener('click', function() {
-    document.getElementById('floorImage').src = 'Web Diplomacy.png';
+    document.getElementById('floorImage').src = 'Third Floor.jpg';
 });
