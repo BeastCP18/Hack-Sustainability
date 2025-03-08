dde15e57434511a4d80eba84f4abd584727d7b40
BeastCP18 <pagetb@gonzaga.org>
2025-03-08 13:39:07 -0500

    Update HomePage.html and HomePage.js to enhance floor selection functionality and update floor images

--- my-crud-app/HomePage.js
+++ my-crud-app/HomePage.js
@@ -1,20 +1,14 @@
-var floorImage1 = new Image();
-floorImage.src = 'images/floor1.png';
-
-var FloorImage2 = new Image();
-FloorImage2.src = 'images/floor2.png';
-
-var FloorImage3 = new Image();
-FloorImage3.src = 'images/floor3.png';
+let floorImage = document.getElementById('floorImage');
+floorImage.src = 'Map of Gonzaga.jpg';
 
 document.getElementById('floor1').addEventListener('click', function() {
-    document.getElementById('floorImage').src = 'images/FirstFloor.jpg';
+    document.getElementById('floorImage').src = 'Map of Gonzaga.jpg';
 });
 
 document.getElementById('floor2').addEventListener('click', function() {
-    document.getElementById('floorImage').src = 'images/SecondFloor.jpg';
+    document.getElementById('floorImage').src = 'Magic the Gathering.webp';
 });
 
 document.getElementById('floor3').addEventListener('click', function() {
-    document.getElementById('floorImage').src = 'images/ThirdFloor.jpg';
+    document.getElementById('floorImage').src = 'Web Diplomacy.png';
 });
